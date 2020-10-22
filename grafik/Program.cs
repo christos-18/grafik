1d6782f872324471327c562f9b5c5c1d9f056e8a
christos <potamitischristos@gmail.com>
2020-10-22 14:25:30 +0200

    1

---
+++ grafik/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Raylib_cs;
+
+namespace grafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Raylib.InitWindow(800, 600, "Jag är bäst");
+
+            float x = 0;
+            float y = 0;
+
+            while(!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                 x += 0.1f;
+                }
+
+             else if(Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+              {
+                    x -= 0.1f;
+                }
+
+                else if(Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    y += 0.1f;
+
+                }
+
+                else if(Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    y -= 0.1f;
+
+                }
+
+
+
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BLUE);
+
+                Raylib.DrawRectangle((int)x, (int)y, 30, 30, Color.WHITE);
+
+
+                Raylib.EndDrawing();
+
+            }
+
+            
+        }
+    }
+}
